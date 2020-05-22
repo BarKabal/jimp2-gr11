587cbc616881179530c648bdbfc46ef2fddd76f8
Skokus <skok2022@gmail.com>
2020-05-22 19:18:07 +0200

    Update Matrix.java

--- WireWorld/src/wireworldfiles/Matrix.java
+++ WireWorld/src/wireworldfiles/Matrix.java
@@ -3,12 +3,17 @@ package wireworldfiles;
 public class Matrix {
 	public int rows;
 	public int collumns;
+	public int iteration;
 	
-	public Cell[][] board;
+	public Cell[][][] board; // Cell[rows][collumns][iteration]
 	
-	Matrix(int rows, int collumns, Cell[][] board){
+	Matrix(int rows, int collumns, Cell[][][] board){
 		this.rows = rows;
 		this.collumns = collumns;
 		this.board = board;
+	}
+	
+	public static void main(int args) {
+		
 	}
 }
