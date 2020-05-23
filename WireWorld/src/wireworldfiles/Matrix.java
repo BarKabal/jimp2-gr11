0649091e3fbb73d6dfdeeae66929cda2204d9574
Skokus <skok2022@gmail.com>
2020-05-23 22:18:06 +0200

    Utworzenie 3 wymiarowej macierzy

--- WireWorld/src/wireworldfiles/Matrix.java
+++ WireWorld/src/wireworldfiles/Matrix.java
@@ -4,42 +4,47 @@ public class Matrix {
 	public int rows;
 	public int collumns;
 	public int iteration;
-	
-	public Cell[][] board; // Cell[rows][collumns]
-	
-	Matrix(int rows, int collumns){
+
+	public Cell[][][] board; // Cell[rows][collumns][iteration]
+
+	Matrix(int rows, int collumns, int iteration) {
 		this.rows = rows;
 		this.collumns = collumns;
-		this.board = new Cell[rows][collumns];
+		this.iteration = iteration;
+		this.board = new Cell[rows][collumns][iteration];
 	}
-	
-	Matrix(int rows, int collumns, Cell board[][]){
+
+	Matrix(int rows, int collumns, Cell board[][][]) {
 		this.rows = rows;
 		this.collumns = collumns;
 		this.board = board;
 	}
-	
+
 	public static void main(String[] args) {
-		Matrix matrix = new Matrix(6,3);		// Dane testowe
-		
+		Matrix matrix = new Matrix(5, 4, 2);
+
 		initializeMatrix(matrix);
-		
 		printMatrix(matrix);
 	}
-	
+
 	public static void printMatrix(Matrix matrix) {
-		for(int i = 0; i < matrix.rows; i++) {
-			for(int j = 0; j < matrix.collumns; j++) {
-				System.out.print(matrix.board[i][j].state + " ");
+		for (int n = 0; n < matrix.iteration; n++) {
+			System.out.println((n+1) + ". Iteracja:");
+			for (int i = 0; i < matrix.rows; i++) {
+				for (int j = 0; j < matrix.collumns; j++) {
+					System.out.print(matrix.board[i][j][0].state + " ");
+				}
+				System.out.println();
 			}
-			System.out.println();
 		}
 	}
-	
+
 	public static void initializeMatrix(Matrix matrix) {
-		for(int i = 0; i < matrix.rows; i++) {
-			for(int j = 0; j < matrix.collumns; j++) {
-				matrix.board[i][j] = new Cell(2);
+		for (int n = 0; n < matrix.iteration; n++) {
+			for (int i = 0; i < matrix.rows; i++) {
+				for (int j = 0; j < matrix.collumns; j++) {
+					matrix.board[i][j][n] = new Cell(2);
+				}
 			}
 		}
 	}
