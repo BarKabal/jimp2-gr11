9b8996bb4724968d10c695d59e25dfb601d27b56
BarKabal <bkabala00@gmail.com>
2020-05-29 22:47:21 +0200

    Animacja działa

--- WireWorld/src/GUI/Window.java
+++ WireWorld/src/GUI/Window.java
@@ -1,14 +1,7 @@
 package GUI;
 
-import loadsavefile.CreateImage;
-import loadsavefile.LoadFile;
-import loadsavefile.SaveFile;
-import wireworldfiles.Matrix;
-
 import javax.swing.*;
-
-import static loadsavefile.CreateImage.MakeImage;
-import static wireworldfiles.Matrix.*;
+import java.awt.*;
 
 public class Window extends JFrame {
     //Color mainBackgroundColor = new Color(253, 204, 125, 221);        Potem to dodam.
@@ -17,16 +10,17 @@ public class Window extends JFrame {
 
     public void run(){
         setBasics();
-        setMenu();
+        //setMenu();
         setAnimation();
         setVisible(true);
     }
 
     private void setBasics() {
         setTitle("WireWorld");
-        setSize(1000,800);
-        setLayout(null);
+        setSize(900,640);
+        setLayout(new BorderLayout());
         setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setResizable(false);
     }
 
     private void setMenu(){
@@ -36,25 +30,9 @@ public class Window extends JFrame {
         setJMenuBar(menuBar);
     }
 
-    public static void main(String[] args){
-        Matrix matrix = LoadFile.loadMatrixSize();
-        Matrix.startMatrix(matrix);
-        LoadFile.loadMatrixState(matrix);
-        for (int n = 0; n < Matrix.DEFAULT_ITERATION; n++) {
-            checkCell(matrix.rows, matrix.columns, n, matrix);
-            printMatrix(n, matrix);
-            initializeMatrix(matrix);
-            if (n + 1 < DEFAULT_ITERATION)
-                changeMatrix(n, matrix);
-            MakeImage(matrix, n);
-        }
-        Window window = new Window();
-        window.run();
-        SaveFile.saveFile(DEFAULT_ITERATION  - 1, matrix);
-    }
-
     public void setAnimation() {
         Animation animation = new Animation();
-        getContentPane().add(animation);
+        animation.setBounds(50,50,600,600);
+        animation.StartAnimation(10, 0, 500, this);
     }
 }
---
+++ WireWorld/src/GUI/WireWorld.java
@@ -0,0 +1,30 @@
+package GUI;
+
+import loadsavefile.LoadFile;
+import loadsavefile.SaveFile;
+import wireworldfiles.Matrix;
+
+import static loadsavefile.CreateImage.MakeImage;
+import static wireworldfiles.Matrix.*;
+import static wireworldfiles.Matrix.DEFAULT_ITERATION;
+
+public class WireWorld {
+
+    public static void main(String[] args){
+        Matrix matrix = LoadFile.loadMatrixSize();
+        Matrix.startMatrix(matrix);
+        LoadFile.loadMatrixState(matrix);
+        for (int n = 0; n < Matrix.DEFAULT_ITERATION; n++) {
+            checkCell(matrix.rows, matrix.columns, n, matrix);
+            printMatrix(n, matrix);
+            initializeMatrix(matrix);
+            if (n + 1 < DEFAULT_ITERATION)
+                changeMatrix(n, matrix);
+            MakeImage(matrix, n);
+        }
+        Window window = new Window();
+        window.run();
+        SaveFile.saveFile(DEFAULT_ITERATION  - 1, matrix);
+    }
+
+}
